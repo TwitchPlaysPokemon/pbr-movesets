39dfd4d8855060b1d1138c8f268d3f81bade0d2a
Felk <studio.felk@googlemail.com>
2016-10-18 03:18:07 +0200

    improved compile.py

--- compile.py
+++ compile.py
@@ -3,6 +3,7 @@ import os
 import sys
 import logging
 import warnings
+import json
 try:
     import yaml
 except ImportError:
@@ -16,7 +17,9 @@ except ImportError:
 
 
 sets = []
-outfile = "compiled.yaml"
+outfile = "compiled.json"
+
+existing_ids = {}
 
 for dir, _, files in os.walk("."):
     for file in files:
@@ -27,17 +30,29 @@ for dir, _, files in os.walk("."):
             try:
                 contents = list(yaml.load_all(f))
                 for set in contents:
+                    if not set:
+                        print("Skipping empty set in {}".format(filepath))
+                        continue
                     identifier = "{}:{} {}".format(filepath, set.get("species"), set.get("setname"))
                     with warnings.catch_warnings(record=True) as w:#
                         try:
-                            sets += [pokecat.populate_pokeset(set)]
-                            for warning in w:
-                                print("{}> WARNING: {}".format(identifier, warning.message))
+                            set = pokecat.populate_pokeset(set)
                         except ValueError as ex:
                             print("{}> ERROR: {}".format(identifier, ex))
+                        else:
+                            for warning in w:
+                                print("{}> WARNING: {}".format(identifier, warning.message))
+                            id = (set["species"]["id"], set["setname"])
+                            if id in existing_ids:
+                                prev_identifier = existing_ids[id]
+                                print("{}> ERROR: combination of species {} ({}) and setname {} already exists ({}), but must be unique!"
+                                      .format(identifier, id[0], set["species"]["name"], id[1], prev_identifier))
+                            else:
+                                existing_ids[id] = identifier
+                                sets += [set]
             except yaml.YAMLError as e:
                 print("Error reading file: {}/{}: {}".format(dir, file, e))
 
 print("Writing output to {}...".format(outfile))
 with open(outfile, "w+", encoding="utf-8") as f:
-    yaml.dump_all(sets, f)
+    json.dump(sets, f)
