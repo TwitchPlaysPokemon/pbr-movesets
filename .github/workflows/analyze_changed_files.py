0b1d3be56feffd7d38a619ddce05aad3f10177d5
felk <de.felix.koenig@gmail.com>
2021-04-24 17:53:01 +0200

    changes_info workflow: replace bash with python

---
+++ .github/workflows/analyze_changed_files.py
@@ -0,0 +1,29 @@
+import json
+import os
+
+import pokecat
+import yaml
+from yaml.cyaml import CLoader
+
+with open(os.path.expandvars("${HOME}/files.json")) as f:
+    changed_files = json.load(f)
+
+for changed_file in changed_files:
+    if not changed_file.startswith("pokesets/") or os.path.splitext(changed_file)[1].lower() not in (".yml", ".yaml"):
+        print("{} is not a pokeset".format(changed_file))
+        continue
+    print("{} is a pokeset".format(changed_file))
+    with open(changed_file) as f:
+        num_lines = sum(1 for line in f)
+    with open(changed_file) as f:
+        raw_sets = list(yaml.load_all(f, Loader=CLoader))
+    info_text = "Stats (Not a warning, just for information):\n"
+    info_text += "| file | set | species | hp | atk | def | spe | spA | spD |\n"
+    info_text += "| --- | --- | --- | --- | --- | --- | --- | --- | --- |\n"
+    for raw_set in raw_sets:
+        pokeset = pokecat.instantiate_pokeset(pokecat.populate_pokeset(raw_set))
+        stats = pokeset["stats"]
+        info_text += "| {} | {} | {} | {} | {} | {} | {} | {} |\n".format(
+            pokeset["setname"], pokeset["species"]["name"],
+            stats["hp"], stats["atk"], stats["def"], stats["spe"], stats["spA"], stats["spD"])
+    print("::warning file={},line={}::{}".format(changed_file, num_lines, info_text.replace("\n", "%0A")))
