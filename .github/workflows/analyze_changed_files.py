34c8e73e67c823e2ecd4d8925f9d56cb59b0e0b5
felk <de.felix.koenig@gmail.com>
2021-08-10 20:08:03 +0200

    skip ev check in analyze workflow
    this is already covered by the compile workflow

--- .github/workflows/analyze_changed_files.py
+++ .github/workflows/analyze_changed_files.py
@@ -19,7 +19,7 @@ for changed_file in changed_files:
         raw_sets = list(yaml.load_all(f, Loader=CLoader))
     info_text = "Stats (Not a warning, just for information):\n"
     for raw_set in raw_sets:
-        pokeset = pokecat.instantiate_pokeset(pokecat.populate_pokeset(raw_set))
+        pokeset = pokecat.instantiate_pokeset(pokecat.populate_pokeset(raw_set, skip_ev_check=True))
         stats = pokeset["stats"]
         info_text += "{} {}: {} hp, {} atk, {} def, {} spe, {} spA, {} spD\n".format(
             pokeset["species"]["name"], pokeset["setname"],
